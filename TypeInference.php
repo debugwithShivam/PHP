cbd55737b7078b4786027a4d6fb0cd885b643e5d
shivam pandey <sp5812070@gmail.com>
2026-06-21 19:43:41 +0530

    PHP TypeInference

---
+++ TypeInference.php
@@ -0,0 +1,12 @@
+<?php
+// Type inference in PHP:
+// PHP infers the variable type from the right-hand side value.
+//
+// 10      -> int
+// 10.5    -> float
+// "hello" -> string
+// true    -> bool
+
+$x = 10;
+var_dump($x); // int(10)
+?>
